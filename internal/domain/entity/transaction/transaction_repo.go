8849e4d8c6a87253f66f9fdafaa054e8fc8815cf
Joer <ra.joer@gmail.com>
2024-07-03 09:08:56 -0300

    refact: remove domain

--- internal/domain/entity/transaction/transaction_repo.go
+++
@@ -1,7 +0,0 @@
-package transaction
-
-type TransactionRepo interface {
-	Get(id string) (Transaction, error)
-	Add(t Transaction) (TransactionRaw, error)
-	Save(t Transaction) (TransactionRaw, error)
-}
