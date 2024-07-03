612a868e8e209af890cfc792f28e7138be051580
Joer <ra.joer@gmail.com>
2024-07-03 09:13:27 -0300

    refact: reset internal

--- internal/domain/entity/transaction/transaction.error.go
+++
@@ -1,9 +0,0 @@
-package transaction
-
-import "errors"
-
-var (
-	ErrInvalidPostingDate     = errors.New("invalid posting date")
-	ErrInvalidTransactionType = errors.New("invalid transaction type")
-	ErrCreatedByIsRequired    = errors.New("created by is required")
-)
